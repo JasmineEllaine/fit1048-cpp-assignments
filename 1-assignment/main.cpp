aaa5b62012abcdfa1b52a480af623f7847a1b9d5
Jasmine Banares <jasminebanares@192-168-1-108.tpgi.com.au>
2019-08-27 22:00:22 +1000

    Added documentation to assignment 1

--- 1-assignment/main.cpp
+++ 1-assignment/main.cpp
@@ -17,9 +17,12 @@
 #include <sstream>
 
 // Global variable declarations.
-std::string lastGeneratedTable;
+std::string lastGeneratedTable = "";
 
 void displayMenu() {
+    /***************************************************************************
+     * Displays a text menu in the console.
+     **************************************************************************/
 	// Display menu header.
 	std::cout << std::endl
         << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl
@@ -37,26 +40,25 @@ void displayMenu() {
 		<< "Select one of the options above (0-5): ";
 }
 
-int getValidInput() {
-	// Initialise user input.
-	int userChoice = -1;
-	std::cin >> userChoice;
-	// Keep asking for input until it is an integer from 0-5.
-	while (userChoice < 0 || userChoice > 5 || std::cin.fail()) {
-		std::cout << "Error: Input only integers from 0-5: ";
-		std::cin.clear();
-		std::cin.ignore(256, '\n');
-		std::cin >> userChoice;
-	}
-	return userChoice;
-}
-
 void displayText(std::string filename) {
-    // Add checks if file does not exist
-    // http://www.fredosaurus.com/notes-cpp/io/readtextfile.html
+    /***************************************************************************
+     * Prints out contents of a file into the console exactly the way it was
+     * formatted.
+     * 
+     * @param   filename    Name of the text file to be displayed.
+     **************************************************************************/
     std::cout << std::endl;
     std::fstream file;
+
     file.open(filename);
+    // Print error message if about file is not found or empty.
+    if (file.peek() == std::ifstream::traits_type::eof()) {
+        std::cout << "Sorry, it seems like the file you're reading"
+            << std::endl << "either doesn't exist is is empty! :(" << std::endl;
+        return;
+    }
+
+    // Display message.
     std::string textline;
     while (std::getline(file, textline)) {
         std::cout << textline << std::endl;
@@ -65,44 +67,26 @@ void displayText(std::string filename) {
 }
 
 std::vector<std::string> createLists(std::string filename) {
-    // Check if file exists.
+    /***************************************************************************
+     * Loads data from a file and returns this data as a vector.
+     * 
+     * @param   filename    Name of the text file to be displayed.
+     * @return  tallyData   String vector containing data from each line of
+     *                      file. Empty if file does not exist or is empty.
+     **************************************************************************/
+    // Initialise empty vector.
+    std::vector<std::string> tallyData;
+
+    // Open file.
     std::ifstream data;
     data.open(filename);
-    while (!data) {
-        std::cout << filename << " does not exist." << std::endl
-        << "Enter a different filename: ";
-        std::cin >> filename;
+
+    // Return empty vector if file not found/empty.
+    if (data.peek() == std::ifstream::traits_type::eof()) {
+        return tallyData;
     }
 
-    // Initialise vector of data
-    // std::vector<int> tallyLineData(14, 0);
-
-    // std::cout << data.eof();
-    // // Return 0 vector if file is empty.
-    // if (!data.eof()) {
-    //     // std::cout << "Empty File" << std::endl;
-    //     return tallyLineData;
-    // }
-
-    // std::string currentData;
-    // std::string delimiter = " ";
-    // // If not empty read data into vector
-    // while (getline(data, currentData)) {
-    //     int strPos = 0;
-    //     int vectorPos = 0;
-    //     std::string currValue;
-    //     // Get each value demlimited by a space.
-    //     while ((strPos = currentData.find(delimiter)) != std::string::npos) {
-    //         currValue = currentData.substr(0, strPos);
-    //         // Insert each value into vector.
-    //         tallyLineData[vectorPos++] = std::stoi(currValue);
-    //         currentData.erase(0, strPos + delimiter.length());
-    //     }
-    //     tallyLineData[vectorPos++] = std::stoi(currentData);
-
-    // }
-
-    std::vector<std::string> tallyData;
+    // Read file into vector if not empty.
     std::string currData;
     while (std::getline(data, currData)) {
         tallyData.push_back(currData);
@@ -112,12 +96,20 @@ std::vector<std::string> createLists(std::string filename) {
 }
 
 int getIntInput(std::string prompt, int lo, int hi) {
+    /***************************************************************************
+     * Asks user for valid integer input.
+     * 
+     * @param   prompt  Prompt to be displayed to the user. Displayed as is.
+     * @param   lo      Lowest integer user can input.
+     * @param   hi      Highest integer user can input.
+     * @return  input   User's input.
+     **************************************************************************/
     std::cout << prompt;
 
-	// Initialise user input.
+	// Initialise user input to be lower than allowed range.
 	int input = lo - 1;
 	std::cin >> input;
-	// Keep asking for input until it is an integer from 0-5.
+	// Keep asking for input until it is within range specified.
 	while (input < lo || input > hi || std::cin.fail()) {
 		std::cout << "Error: Input only integers from " << lo << "-"
             << hi << ": ";
@@ -129,6 +121,17 @@ int getIntInput(std::string prompt, int lo, int hi) {
 }
 
 std::string vectorToString(std::vector<int> vector) {
+    /***************************************************************************
+     * Converts a vector into a string where each element of a vector is
+     * separated by a space. E.g. {1, 2, 3} -> "1 2 3".
+     * 
+     * @param   vector  Vector to be stringified.
+     * @return  output  Stringified vector.
+     * 
+     * Preconditions:
+     * - vector must not be empty.
+     **************************************************************************/
+    // Initialise output string using the first elem of vector.
     std::string output = std::to_string(vector[0]);
     for (int i = 1; i < vector.size(); i++) {
         output += " " + std::to_string(vector[i]);
@@ -136,65 +139,98 @@ std::string vectorToString(std::vector<int> vector) {
     return output;
 }
 
-std::vector<int> stringToVector(std::string string, std::string delimiter) {
-    // int strPos = 0;
-    // int vectorPos = 0;
-    // std::string currValue;
-    // // Get each value demlimited by a space.
-    // while ((strPos = string.find(delimiter)) != std::string::npos) {
-    //     currValue = string.substr(0, strPos);
-    //     // Insert each value into vector.
-    //     output[vectorPos++] = std::stoi(currValue);
-    //     string.erase(0, strPos + delimiter.length());
-    // }
-    // output[vectorPos++] = std::stoi(string);
-    std::vector<int> output(14, 0);
+std::vector<int> stringToVector(std::string string) {
+    /***************************************************************************
+     * Splits a string into a vector<int>delimited by a " ".
+     * E.g. "1 2 3" -> {1, 2, 3}.
+     * 
+     * @param   string  String to be split.
+     * @return  output  Vector containing integers split using a delimiter.
+     * 
+     * Preconditions:
+     * - vector must not be empty.
+     **************************************************************************/
+    // Initialise output vector.
+    std::vector<int> output;
     std::istringstream is(string);
     int currValue;
-    int counter = 0;
     while (is >> currValue) {
-        output[counter++] = currValue;
+        output.push_back(currValue);
     }
     return output;
 }
 
 std::string generateTable(int diceUsed, int diceRolls) {
+    /***************************************************************************
+     * Generates a table with resuls of dice rolls specified by user.
+     * 
+     * @param   diceUsed        The number of dice used.
+     * @param   diceRolls       The number of times dice is rolled divided by
+     *                          10.
+     * @return  tallyResults    String holding information about this run.
+     *                          First number in string holds the number of dice
+     *                          used, second number in the string holds the
+     *                          number of times dice was rolled, and the
+     *                          remaining rerpresents the number of times a
+     *                          certain number was obtained. E.g.
+     *                          "1 3 5 6 7 1 2 4 0 0 0 0 0 0"
+     *                          1 = dice used
+     *                          3 = times dice rolled
+     *                          5 = number of times "1" was rolled
+     *                          6 = number of times "2" was rolled
+     *                          7 = number of times "3" was rolled etc...
+     * 
+     * Postcondition:
+     * - tallyResults has 14 substrings all separeted by a space.
+     **************************************************************************/
+    // Initialise a vector of size 14 each containing 0.
     std::vector<int> tallyResults(14, 0);
     tallyResults[0] = diceUsed;
     tallyResults[1] = diceRolls;
 
+    // Roll dice diceRolls*10 amount of times.
     for (int i = 0; i < diceRolls*10; i++) {
         int diceResultTotal = 0;
+        // Rolls dice either once or twice.
         for (int j = 0; j < diceUsed; j++) {
-            // Roll a die.
             int dieResult = std::rand() % 6 + 1;
+            // Adds current dice roll to total.
+            // Significant if there is more than 1 die used.
             diceResultTotal += dieResult;
         }
-        // Minus one because indices start at 0.
+
         // First two items in vector reserved for dice used and rolls.
+        // Hence, an "n" dice roll will be the n+1 element of the vector.
         tallyResults[diceResultTotal+1]++;
     }
-
-    // for (int i = 0; i < tallyResults.size(); i++){
-    //     std::cout << tallyResults[i] << std::endl;
-    // }
-
-    tallyResults[0] = diceUsed;
-    tallyResults[1] = diceRolls;
 
     return vectorToString(tallyResults);
 }
 
 void printTableFromVector(std::vector<int> tableData) {
+    /***************************************************************************
+     * Prints a result table using data from a vector.
+     * 
+     * @param   tableData   14-element vector containing results and info from
+     *                      die rolling experiment.
+     * 
+     * Precondition:
+     * - tableData follows a similar format to tallyResults above. I.E. first
+     *   two elements are information on how many dice used and how many times
+     *   it was rolled, and then the rest are information on how many times each
+     *   number was rolled.
+     **************************************************************************/
     std::cout << std::endl;
     std::cout << "You rolled " << tableData[0] << " dice " << tableData[1]
         << "0 times." << std::endl;
     std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
     
-    // Range of dice rolls.
+    // Range of dice rolls. Used to display 1-6 only for experiments where only
+    // one die is used, and display 2-12 is 2 were used.
     int lo = tableData[0] < 2 ? 1 : 2;
     int hi = lo == 1 ? 6 : 12;
-    for (lo; lo <= hi; lo++) { 
+    for (lo; lo <= hi; lo++) {
+        // Formatting: Add extra space before number if single digit.
         if (lo < 10) {
             std::cout << " ";
         }
@@ -203,7 +239,16 @@ void printTableFromVector(std::vector<int> tableData) {
     }
 }
 
-void saveToFile(std::string filename, std::string lineToWrite) {
+void appendToFile(std::string filename, std::string lineToWrite) {
+    /***************************************************************************
+     * Appends a single line of text into a file.
+     * 
+     * @param   filename    Name of the file to be edited.
+     * @param   lineToWrite String to be appended on to file.
+     * 
+     * Precondition:
+     * - lineToWrite cannot be empty.
+     **************************************************************************/
     std::ofstream savedFile;
     savedFile.open(filename, std::ios_base::app);
     savedFile << lineToWrite << "\n";
@@ -211,27 +256,69 @@ void saveToFile(std::string filename, std::string lineToWrite) {
 }
 
 void displayAllDataFromFile(std::string filename) {
+    /***************************************************************************
+     * Displays dice data stored in a file where each line is a stringified
+     * format of tallyResults in the function generateTable.
+     * 
+     * @param   filename    Name of the file containing data.
+     * 
+     * Precondition:
+     * - File must be in the format specified above.
+     **************************************************************************/
+    // Load data.
     std::vector<std::string> tallyData = createLists(filename);
+    
+    // Handle empty/non-existent file.
+    if (tallyData.empty()) {
+        std::cout << std::endl << "Error: Data can't be loaded as no tables"
+            << std::endl << "have been saved yet." << std::endl;
+            return;
+    }
+    
+    // Print each table.
     for (int i = 0; i < tallyData.size(); i++) {
-        // std::cout << tallyData[i];
-        // std::cin >> i;
-        printTableFromVector(stringToVector(tallyData[i], " "));
+        printTableFromVector(stringToVector(tallyData[i]));
     }
 }
 
 bool processChoice (int userChoice) {
+    /***************************************************************************
+     * Processes user input to show correct functionality.
+     * 
+     * @param   userChoice  User's choice.
+     * @return  bool        false if user wishes to exit program,
+     *                      true otherwise.
+     * 
+     * Precondition:
+     * - userChoice must be from 0-5.
+     **************************************************************************/
     // Switch statement has the precondition that the numbers are from 0-5
     switch (userChoice) {
-        case 1: displayText("TallyAbout.txt");
+        case 1: // Case 1: Displays information about the program.
+                displayText("TallyAbout.txt");
                 system("pause");
                 break;
         case 2: {
+                // Case 2: Loads and stores data from a file.
                 std::vector<std::string> tallyData = 
                     createLists("savedTallyData.txt");
+                // Handle empty/non-existent file.
+                if (tallyData.empty()) {
+                    std::cout << std::endl
+                        << "Error: Data can't be loaded as no tables"
+                        << std::endl << "have been saved yet." << std::endl;
+                } else {
+                    // Latest item in the file is loaded into last generated
+                    // table as per Gavin's response in the forum.
+                    lastGeneratedTable = tallyData.back();
+                }
                 system("pause");
                 break;
             }
         case 3: {
+                // Case 3: Generates a table of results from the experiment
+                // based on user's specified conditions.
+                std::cout << std::endl;
                 int diceUsed = getIntInput("How many dice to roll (1-2): ",
                                            1,
                                            2);
@@ -239,25 +326,41 @@ bool processChoice (int userChoice) {
                                            1,
                                            10);
                 lastGeneratedTable = generateTable(diceUsed, diceRolls);
-                printTableFromVector(stringToVector(lastGeneratedTable, " "));
+                // Converts stringified table results to vector first before
+                // printing because printing from a vector was more efficient
+                // than printing from a string.
+                printTableFromVector(stringToVector(lastGeneratedTable));
                 system("pause");
                 break;
         }
         case 4: {
-                printTableFromVector(stringToVector(lastGeneratedTable, " "));
-                std::cout << std::endl
-                    << "Do you want to save this data - (y/n): ";
-                std::string userChoice;
-                std::cin >> userChoice;
-                if (userChoice == "y") {
-                    saveToFile("savedTallyData.txt", lastGeneratedTable);
+                // Case 4: Save last generated table to a file.
+                // Checks first if a table has been generated. Prints a message
+                // if not.
+                if (lastGeneratedTable == "") {
+                    std::cout << std::endl << "Generate a table first before"
+                        << "saving to a file." <<std::endl;
+                } else {
+                    // Prints last generated table.
+                    printTableFromVector(stringToVector(lastGeneratedTable));
+                    // Prompts user if they want to save the last generated
+                    // table.
                     std::cout << std::endl
-                        << "These statistics are now saved." << std::endl;
+                        << "Do you want to save this data - (y/n): ";
+                    std::string userChoice;
+                    std::cin >> userChoice;
+                    // Handles user choice.
+                    if (userChoice == "y") {
+                        appendToFile("savedTallyData.txt", lastGeneratedTable);
+                        std::cout << std::endl
+                            << "These statistics are now saved." << std::endl;
+                    }
                 }
                 system("pause");
                 break;
         }
-        case 5: displayAllDataFromFile("savedTallyData.txt");
+        case 5: // Case 5: Displays all experiment date stored in file.
+                displayAllDataFromFile("savedTallyData.txt");
                 system("pause");
                 break;
     }
@@ -267,6 +370,7 @@ bool processChoice (int userChoice) {
 }
 
 int main() {
+    // Initialise program to run.
     bool runProgram = true;
     while (runProgram) {
         displayMenu();
